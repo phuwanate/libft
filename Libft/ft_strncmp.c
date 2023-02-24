bf3cd8e73e4d0a92118fea38bc7d227ed07a65bd
Phuwanate Lertsiriyothin <plertsir@z2t3c4.42bangkok.com>
2023-02-24 20:33:46 +0700

    add file from 42 24022023

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -15,19 +15,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
 
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
 	i = 0;
-	while ((ptr1[i] || ptr2[i]) && (n > 0))
+	while ((ptr1[i] || ptr2[i]) && (n-- > 0))
 	{
 		if (ptr1[i] != ptr2[i])
 			return (ptr1[i] - ptr2[i]);
 		i++;
-		n--;
 	}
 	return (0);
 }
