385c4f5cdb3a67fb586e73939c19ef70b9642ae2
Firstphu <phuwanate.lpk@gmail.com>
2023-03-31 23:47:39 +0700

    add file from home 310323

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: plertsir <plertsir@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 16:03:16 by plertsir          #+#    #+#             */
-/*   Updated: 2023/03/08 16:08:29 by plertsir         ###   ########.fr       */
+/*   Updated: 2023/03/31 23:42:10 by first            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	n;
 
-	if (count == SIZE_MAX || size == SIZE_MAX)
+	n = count * size;
+	if (count == SIZE_MAX || size == SIZE_MAX || (n != count * size))
 		return (NULL);
 	ptr = (void *)malloc(count * size);
 	if (ptr)
